4f696ad629f59671ac3a7adc6e4407bb903e1606
mikel13200 <mikel.dadula03@gmail.com>
2022-06-08 10:06:37 +0800

    AariseGod Ping Kick

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.CheckPing = 5 -- default ping kick ay 150 kayo na bahala mag adjust
+Config.CheckInterval = 5000 -- kung alam mo ano ginagawa mo goods lang, pero kapag di mo alam, wag mo nalan galawin to
+Config.CheckWarning = "Your ping is too high. Fix it. (%dms, Warning: %d/3)" -- Warning sa mga player
+Config.KinickSiBobo = "You were kicked for having a high ping. (%dms)" -- kapag na kick at may notif sa fivem
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'common'
+
+author 'AriseGod#0001'
+description 'BASTA POGI AKO'
+
+server_script {
+    'config.lua',
+    'server.lua'
+}
+
+server_only 'yes'
